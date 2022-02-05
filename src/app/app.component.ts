f47ea2949f9cbde6be31c9c18304234a5413848b
Tommi022 <falcione.tommaso@iisgalvanimi.edu.it>
2022-02-05 16:23:08 +0000

    Youtube Player Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <h1>Youtube Player</h1>
   <div>
     <youtube-player 
       videoId="GYAB4Td62zI" 
@@ -12,7 +13,36 @@ import { Component } from '@angular/core';
       [startSeconds]="4"
       [endSeconds]="8">
     </youtube-player>
-  </div>  
+
+    <youtube-player 
+      videoId="6OUDk4gMFLA" 
+      suggestedQuality="highres" 
+      [height]="250" 
+      [width]="500" 
+      [startSeconds]="4"
+      [endSeconds]="8">
+    </youtube-player>
+  </div> 
+  <br> 
+  <div>
+    <youtube-player 
+      videoId="GYAB4Td62zI" 
+      suggestedQuality="highres" 
+      [height]="250" 
+      [width]="500" 
+      [startSeconds]="4"
+      [endSeconds]="8">
+    </youtube-player>
+
+    <youtube-player 
+      videoId="6OUDk4gMFLA" 
+      suggestedQuality="highres" 
+      [height]="250" 
+      [width]="500" 
+      [startSeconds]="4"
+      [endSeconds]="8">
+    </youtube-player>
+  </div>
   `,
   styleUrls: ['./app.component.css']
 })
